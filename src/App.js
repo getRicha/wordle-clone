75f023feb481533e88e03551f5c22bf0b2a19cb9
getRicha <richasri2002@gmail.com>
2022-08-04 23:47:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import { boardDefault } from './words';
+import Keyboard from './components/Keyboard';
+import {createContext, useState} from 'react';
 
+// context API lets us pass data through the component tree without having to pass props down manually at every level
+// layman's way of saying: "I want to pass this data down to all of my children"
+// we can use the useContext hook to access the context
+export const AppContext = createContext();
+
+//access to both the states anywhere under the provider 
 function App() {
+  const [board, setBoard] = useState(boardDefault)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{board, setBoard}}>
+        <div className='game'>
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
     </div>
   );
 }
