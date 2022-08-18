7c1591a3054e771a0b77232e12792d2f74d394c7
getRicha <richasri2002@gmail.com>
2022-08-18 23:44:36 +0530

    Update key.js with react hook

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {AppContext} from '../App';
 
 function Key({keyVal, bigKey}){
+    const {onDelete, onSelectLetter, onEnter} = useContext(AppContext);
+    const selectLetter = () => {
+        if(keyVal === 'Enter'){
+            onEnter()
+        }else if(keyVal === 'Delete'){
+            onDelete()
+        }else{
+            onSelectLetter(keyVal)
+        }
+    }
     return (
-        <div className='key' id={bigKey && 'big'}>{keyVal}</div>
+        <div className='key' id={bigKey && 'big'} onClick={selectLetter}>{keyVal}</div>
     )
 }
 
